57b422ec22a79a8c82f721414ce7a4e30bb09326
Akhila Vasa <akhila.vasa@gmail.com>
2021-12-04 19:58:36 +0530

    first commit

---
+++ src/pages/Editprofile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Editprofile() {
+
+   
+
+  return (
+    <div className='editprofile'>
+      <h1>Edit profile</h1>
+    </div>
+  );
+}
+
+export default Editprofile;
---
+++ src/pages/Mainpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Mainpage() {
+  return (
+    <div className='mainpage'>
+       <h1>REACT CRUD OPERATION </h1>
+               
+       
+       
+    </div>
+  );
+}
+
+export default Mainpage;
